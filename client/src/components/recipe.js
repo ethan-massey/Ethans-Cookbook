b6732f0e59daf3623a3e9b0cd9640a10a83f8185
Ethan Massey <ejm4aa@virginia.edu>
2022-08-18 00:21:36 -0400

    indents and styling i think. Not sure why these keep showing in my changes

--- client/src/components/recipe.js
+++ client/src/components/recipe.js
@@ -1,79 +1,83 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import BackEditDeleteButtons from "./backEditDeleteBar";
- 
-export default function Recipe(props) {
- const [recipeData, setRecipeData] = useState({});
 
- let params = useParams();
- const navigate = useNavigate();
- 
- // This method fetches the records from the database.
-useEffect(() => {
+export default function Recipe(props) {
+  const [recipeData, setRecipeData] = useState({});
+
+  let params = useParams();
+  const navigate = useNavigate();
+
+  // This method fetches the records from the database.
+  useEffect(() => {
     async function getRecipeData() {
-        await fetch(`https://ethans-cookbook.herokuapp.com/api/recipe/${params.id.toString()}`)
-            .then((response) => {
-                if (!response.ok) {
-                    // const message = `An error occurred: ${response.statusText}`;
-                    // window.alert(message);
-                    navigate("/404");
-                    return;
-                }else{
-                    return response.json();
-                }
-            })
-            .then((responseJson) => {
-                setRecipeData(responseJson);
-            })
-            .catch(err => {
-                console.log(err);
-            })
+      await fetch(`https://ethans-cookbook.herokuapp.com/api/recipe/${params.id.toString()}`)
+        .then((response) => {
+          if (!response.ok) {
+            // const message = `An error occurred: ${response.statusText}`;
+            // window.alert(message);
+            navigate("/404");
+            return;
+          } else {
+            return response.json();
+          }
+        })
+        .then((responseJson) => {
+          setRecipeData(responseJson);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     }
 
     getRecipeData();
 
     return;
-}, [params, navigate]);
+  }, [params, navigate]);
 
- function formatIngredients() {
-    if (recipeData.ingredients){
-        return recipeData.ingredients.map((i, index) => {
-            return (
-                <li key={i.name.toString() + index.toString()}>
-                    <p>{i.name} - {i.quantity}</p>
-                </li>
-            )
-        });
+  function formatIngredients() {
+    if (recipeData.ingredients) {
+      return recipeData.ingredients.map((i, index) => {
+        return (
+          <li key={i.name.toString() + index.toString()}>
+            <p>
+              {i.name} - {i.quantity}
+            </p>
+          </li>
+        );
+      });
     }
- }
+  }
 
- function formatSteps() {
-    if (recipeData.steps){
-        return recipeData.steps.map((step, index) => {
-            return (
-                <li key={step.toString() + index.toString()}>
-                    <p>{step}</p>
-                </li>
-            )
-        });
+  function formatSteps() {
+    if (recipeData.steps) {
+      return recipeData.steps.map((step, index) => {
+        return (
+          <li key={step.toString() + index.toString()}>
+            <p>{step}</p>
+          </li>
+        );
+      });
     }
- }
- 
- // This following section will display the table with the records of individuals.
- return (
-<div>
-    <div className="center-text">
+  }
+
+  // This following section will display the table with the records of individuals.
+  return (
+    <div>
+      <div className="center-text">
         <h3 className="recipe-title">{recipeData.name}</h3>
         <h4>Ingredients</h4>
-        <ul>
-            {formatIngredients()}
-        </ul>
+        <ul>{formatIngredients()}</ul>
         <h4>Steps</h4>
-        <ol>
-            {formatSteps()}
-        </ol>
-        <BackEditDeleteButtons backPath="/" recipeId={params.id.toString()} userStatus={props.userStatus} setUserStatus={props.setUserStatus} showDeleteModal={props.showDeleteModal}/>
+        <ol>{formatSteps()}</ol>
+        <BackEditDeleteButtons
+          backPath="/"
+          recipeId={params.id.toString()}
+          userStatus={props.userStatus}
+          setUserStatus={props.setUserStatus}
+          showDeleteModal={props.showDeleteModal}
+        />
+      </div>
     </div>
-</div>
- );
+  );
 }
