85865669a02887174d88bfa128f54a70ac27a87a
Ethan Massey <ejm4aa@virginia.edu>
2022-10-06 16:59:49 -0400

    Revert "Revert "replaced prod calls with localhost calls in dev""
    This reverts commit eabc82ff5bf9f9112e91310577794f8a60ae76ed.
    
    replace prod calls with localhost calls in dev

--- client/src/components/editRecipe.js
+++ client/src/components/editRecipe.js
@@ -24,7 +24,7 @@ export default function EditRecipeForm() {
   useEffect(() => {
     async function getRecipeData() {
       const response = await fetch(
-        `https://ethans-cookbook.herokuapp.com/api/recipe/${params.id.toString()}`
+        `http://localhost:5000/api/recipe/${params.id.toString()}`
       );
 
       if (!response.ok) {
@@ -129,7 +129,7 @@ export default function EditRecipeForm() {
 
   async function submit(recipe) {
     const response = await fetch(
-      `https://ethans-cookbook.herokuapp.com/api/update/${params.id.toString()}`,
+      `http://localhost:5000/api/update/${params.id.toString()}`,
       {
         method: "POST",
         body: JSON.stringify({
